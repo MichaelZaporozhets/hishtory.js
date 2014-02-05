3419c800149808cc64afb1ee3f651fbb694731a8
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2014-02-05 20:31:47 +1100

    Fix everything

--- release/0.2/simpler-history.0.2.js
+++ release/0.2/simpler-history.0.2.js
@@ -27,7 +27,7 @@ SH.history.currentPageArgs = [];
 SH.history.stop = false;
 SH.open = function(name) {
 	if(SH.history.pushState) {
-		window.history.pushState('', name, SH.info.root+name);
+		window.history.pushState('', name, '/'+SH.info.root+name);
 	} else {
 		document.location.hash = '#!/' + name;
 	}
@@ -87,7 +87,7 @@ SH.history.manage = function() {
 			SH.open(hash.substr(3));
 			return false;
 		}
-		if(hash.length > 1 && window.location.pathname == '/') {
+		if(hash.length > 1 && window.location.pathname.substr(1) == SH.info.root) {
 			if(hash.substr(0,3) == '#!/') {
 				hash = hash.substr(3).split('#')[0];
 				if(hash.split(':')[1] !== 'undefined') {
@@ -99,12 +99,13 @@ SH.history.manage = function() {
 				hash = hash.split('/')[0];
 				var args = document.location.hash.substr(3).split('/');
 				if(document.location.hash.substr(3) !== SH.info.pageArgs.join('/')) {
+					var state = hash;
 				  	manangeChange();
 				}
 			}
-		} else if(window.location.pathname !== '/') {
+		} else if(window.location.pathname.replace(SH.info.root,'') !== '/') {
 			SH.history.stop = true;
-			window.location.assign( '/#!/' +window.location.pathname.substr(1));
+			window.location.assign('/'+SH.info.root+'#!/' +window.location.pathname.replace(SH.info.root,'').substr(1));
 		} else {
 			SH.info.page = 'home';
 			SH.open('home');
@@ -122,29 +123,30 @@ SH.history.patrol = function(callback) {
 	callback(true);
 };
 
-SH.init = function(refOpenFunc,pushState) {
-	var pushState = pushState;
-	//to override or not to override
-	if(typeof pushState == 'undefined') { pushState = true; }
-	SH.history.pushState = false;
-	if (history.pushState && pushState == true) {
-		SH.history.pushState = true;
-	}
-	if(typeof refOpenFunc == 'undefined') {
-		console.log('Simpler History: You must declare the main handler as the first argument of SH.init.');
-	} else {
-		SH.refOpenFunc = refOpenFunc;
-		SH.history.patrol(function(test) {
-			if(test == true) {
-				console.log('Simpler History Initialized...');
-			}
-		});
-	}
-	return;
-};
 
-var openPage = function(page) {
-	console.log('do some logic for the page: '+page);
-	return false;
+SH.init = function(refOpenFunc,options) {
+	var soItBegins = function(refOpenFunc,options) {
+		var pushState = options.forceFallback;
+		//to override or not to override
+		if(typeof pushState == 'undefined') { pushState = true; }
+		SH.history.pushState = false;
+		if (history.pushState && pushState == false) {
+			SH.history.pushState = true;
+		}
+		if(typeof refOpenFunc == 'undefined') {
+			console.log('Simpler History: You must declare the main handler as the first argument of SH.init.');
+		} else {
+			SH.refOpenFunc = refOpenFunc;
+			SH.history.patrol(function(test) {
+				if(test == true) {
+					console.log('Simpler History Initialized...');
+				}
+			});
+		}
+		return;
+	}
+	
+	window.addEventListener ? 
+	window.addEventListener("load",function() { soItBegins(refOpenFunc,options) },false) : 
+	window.attachEvent && window.attachEvent("onload",function() { soItBegins(refOpenFunc,options) });
 };
-SH.init(openPage,true);
